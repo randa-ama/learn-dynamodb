8ebe5261c91555a216c0dc13f1f2c81d987f160c
Neal Magee <nem2p@virginia.edu>
2026-04-03 11:55:36 -0400

    Simplify before class

--- hands-on/simple/get-user.py
+++ hands-on/simple/get-user.py
@@ -1,9 +1,1 @@
-import boto3
-
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('users')
-
-response = table.get_item(Key={"username": "jsmith"})
-
-item = response['Item']
-print(item)
+# get a user by username
--- hands-on/simple/scan-users.py
+++ hands-on/simple/scan-users.py
@@ -1,14 +1,1 @@
-import boto3
-
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('users')
-
-# When scanning the limits are:
-# - 1MB of data
-# - Scan can set a limit on the number of items to return
-response = table.scan()
-
-for item in response['Items']:
-    print(item)
-
-print(f"\nTotal items: {response['Count']}")
+# scan for all users
